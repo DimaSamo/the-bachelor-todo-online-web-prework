ee53c7ba15dbf53e97be233c148ff3b7425ca1e5
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-12 02:47:18 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |season_num, cont_array|
     if season_num == season
       cont_array.each do |cont_hash|
-        if cont_hash.fetch("status") == Winner
+        if cont_hash.fetch("status") == "Winner"
           return cont_hash.fetch("name")
         end
       end
