706427c3d23616c8a151655181807ce621f9f993
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-12 02:50:23 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,7 +11,13 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_num, cont_array|
+    cont_array.each do |cont_hash|
+      if cont_hash.fetch("occupation") == occupation
+        return cont_hash.fetch("name")
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
